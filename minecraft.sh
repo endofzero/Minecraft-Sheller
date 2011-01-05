a25308b62875b5bce6f6e73b98c3dbf63cd91cf1
Ripley Jonez <endofzero@gmail.com>
2011-01-05 11:52:28 -0700

    Re-added server options

--- minecraft.sh
+++ minecraft.sh
@@ -13,7 +13,8 @@ SCREEN_NAME="minecraft"
 MEMMAX=1536
 MEMALOC=1024
 DISPLAY_ON_LAUNCH=0
-SERVER_OPTIONS=""
+SERVER_OPTIONS="-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts"
+# SERVER_OPTIONS=""
 
 # Modifications
 SERVERMOD=1
